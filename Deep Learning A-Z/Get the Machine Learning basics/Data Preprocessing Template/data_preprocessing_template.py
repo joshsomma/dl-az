43d0d9e82a65f5fd642dd910bb6f0e7272f60bd5
Joshua Somma <joshuasomma@Joshuas-MacBook-Air.local>
2017-05-14 22:57:42 +0100

    initial commit

---
+++ Deep Learning A-Z/Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 14 22:42:48 2017
+
+@author: joshuasomma
+"""
+
+# data pre-processing
+
+# import libs
+
+# maths operations (particularly arrays)
+import numpy as np
+# data/chart visualiser
+import matplotlib.pyplot as plt
+# import and manage large datasets
+import pandas as pd
