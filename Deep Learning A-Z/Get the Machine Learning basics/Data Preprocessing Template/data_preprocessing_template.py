6d1b6aab319e0207d529b512c1d80a9fa47d50a3
Josh S <josh@joshsomma.com>
2017-05-15 15:28:42 +0100

    finished setting up the data pre-processing template with copius comments

--- Deep Learning A-Z/Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template.py
+++ Deep Learning A-Z/Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_template.py
@@ -6,7 +6,7 @@ Created on Sun May 14 22:42:48 2017
 @author: joshuasomma
 """
 
-# data pre-processing
+# ------> data pre-processing template <-------
 
 # import libs
 
@@ -18,14 +18,14 @@ import matplotlib.pyplot as plt
 # import and manage large datasets
 import pandas as pd
 
-# import the dataset
-
+# --->import the dataset<---
 dataset = pd.read_csv('Data.csv')
 # extract the first 3 cols of dataset and put them into an array; these are the independant variables
 X = dataset.iloc[:,:-1].values
+# extract the dependent variable and place into its own array
 y = dataset.iloc[:,-1].values
                 
-# take care of missing data
+# --->take care of missing data<---
 # removing rows w missing data is bad practice so we will calculate the mean of the column and replace the missing data with this value
 # import Imputer library
 from sklearn.preprocessing import Imputer
@@ -36,7 +36,7 @@ imputer = imputer.fit(X[:,1:3])
 # replace missing data points with the mean of the column
 X[:,1:3] = imputer.transform(X[:,1:3])
 
-# encoding categorical data
+# --->encoding categorical data<---
 # LabelEncoder is used to convert text category labels  (like 'France' etc.) into numeric values
 # import the lib
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -54,4 +54,23 @@ X = onehotencoder.fit_transform(X).toarray()
 # Do the same for the dependant variable column y/n
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
+
+# --->splitting the data between training and test sets<---
+# We need to be able to test the predictions using some uncorellated data so we split out 20% of the table to use as test data once the system is trained on the train data
+from sklearn.cross_validation import train_test_split
+# set up some new vars and set up params for test_size and random_state; random state is used to normalise the numbers
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state = 0)
+
+# --->feature scaling<---
+# different features might have different ranges; i.e. age v salary, you might have 25-50 age range but 25k to 90k salary range; the range on numbers are out of sync and need to be normalised
+# without scaling the features, the larger range will dominate the calculations
+# based on Euclidian geometry and calculating distance between two points
+
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+
+
 
